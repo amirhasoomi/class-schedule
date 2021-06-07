773d9279b61b184b6b979ae475642956f30252c2
Amirhossein Hasoomi <a.h.hasoomi@gmail.com>
2021-06-07 21:45:50 +0430

    validate members of proposal

--- projection/serializers.py
+++ projection/serializers.py
@@ -13,6 +13,18 @@ class CreateProposalSerializer(serializers.ModelSerializer):
                   'leader', 'members', 'file', 'judges', 'register_date')
         read_only_fields = ('pk', 'unique_code', 'register_date', 'judges',)
 
+    def validate(self, attrs):
+        members = attrs['members']
+        leader = attrs['leader']
+        for member in members:
+            if not member.user_type == AuthConf.USER_TYPE_MEMBER:
+                raise serializers.ValidationError(
+                    dict(members=[f'{member.pk} is not a member!', ]))
+            elif member == leader:
+                raise serializers.ValidationError(
+                    dict(members=['leader can not be a member!', ]))
+        return super().validate(attrs)
+
     def create(self, validated_data):
         validated_data['unique_code'] = get_random_string(length=10).upper()
         return super().create(validated_data)
@@ -59,5 +71,4 @@ class AddJudgeSerializer(serializers.ModelSerializer):
             if not judge.user_type == AuthConf.USER_TYPE_JUDGE:
                 raise serializers.ValidationError(
                     dict(judges=[f'{judge.pk} is not a judge!', ]))
-            else:
-                return super().validate(attrs)
+        return super().validate(attrs)
